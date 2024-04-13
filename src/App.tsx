a72df03c1c334ccfd0e9a6f97ac33ea7c83cbb96
baidikov-roman-mi <wolkdem@gmai.com>
2024-04-13 22:10:20 +0300

    update: new dependencies and basic changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DOMPurify from "dompurify"
+import ReactHtmlParser from "react-html-parser"
+import { useRef, useState } from "react"
+import "./index.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [addWord, setAddWord] = useState(false)
+  const [data, setData] = useState<string[]>([])
+
+  const input = useRef<HTMLInputElement>(null)
+
+  function inputValidation() {
+    if (input.current) {
+      const newWord = `<div class="bg-vio-light h-5r pad-2r">${input.current.value}</div>`
+      const sanitizedData = DOMPurify.sanitize(newWord)
+      setData([...data, sanitizedData])
+      setAddWord(false)
+      return sanitizedData
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Your best vocabulary</h1>
+      <button onClick={() => setAddWord(true)}>Add word</button>
+      {addWord && (
+        <div className="dialog-overlay">
+          <dialog open className="bg-vio-mid border-0 border-r-10p pad-2r">
+            <p className="fsize-20p">Now let's add your first word!</p>
+            <div className="flex j-center a-center gap-20p">
+              <input
+                type="text"
+                ref={input}
+                className="border-3 border-vio-dark border-r-10p pad-1r outline-n fsize-1r bg-vio-light"
+                placeholder="New epic word"
+              />
+              <input
+                type="text"
+                ref={input}
+                className="border-3 border-vio-dark border-r-10p pad-1r outline-n fsize-1r bg-vio-light"
+                placeholder="In your native language"
+              />
+              <button
+                className="border-0 border-r-10p pad-1r bg-vio-dark"
+                onClick={() => inputValidation()}
+              >
+                Add
+              </button>
+            </div>
+          </dialog>
+        </div>
+      )}
+      <div className="flex j-center a-center flex-w-wrap gap-20p">
+        {ReactHtmlParser(data.join(""))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
