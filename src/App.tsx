2185af359d0a2ec3b09e0c35a82d64281a03563b
baidikov-roman-mi <wolkdem@gmai.com>
2024-04-20 23:59:05 +0300

    update: add word's meaning, changed word's inserting way

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,75 @@
-import DOMPurify from "dompurify"
-import ReactHtmlParser from "react-html-parser"
 import { useRef, useState } from "react"
 import "./index.css"
 
+interface WordData {
+  word: string
+  meaning: string
+}
+
 const App = () => {
-  const [addWord, setAddWord] = useState(false)
-  const [data, setData] = useState<string[]>([])
+  // example array
+  const exampleArray = [
+    { word: "hello", meaning: "Привіт" },
+    { word: "hello", meaning: "Привіт" }
+  ]
+  // states
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const [data, setData] = useState<WordData[]>(exampleArray)
 
-  const input = useRef<HTMLInputElement>(null)
+  // refs
+  const inputWord = useRef<HTMLInputElement | null>(null)
+  const inputMeaning = useRef<HTMLInputElement | null>(null)
 
-  function inputValidation() {
-    if (input.current) {
-      const newWord = `<div class="bg-vio-light h-5r pad-2r">${input.current.value}</div>`
-      const sanitizedData = DOMPurify.sanitize(newWord)
-      setData([...data, sanitizedData])
-      setAddWord(false)
-      return sanitizedData
-    }
+  // functions
+  function refreshWords() {
+    setShowModal(false)
+    setData((prevData) => [
+      ...prevData,
+      { word: inputWord.current!.value, meaning: inputMeaning.current!.value }
+    ])
   }
 
   return (
     <>
       <h1>Your best vocabulary</h1>
-      <button onClick={() => setAddWord(true)}>Add word</button>
-      {addWord && (
-        <div className="dialog-overlay">
-          <dialog open className="bg-vio-mid border-0 border-r-10p pad-2r">
+      <button
+        className="border-1 border-vio-dark border-r-5p pad-1r bg-vio-dark"
+        onClick={() => setShowModal(true)}
+      >
+        Add word
+      </button>
+
+      {showModal && (
+        <div className="flex-center bg-black-75 pos-fix left-0 top-0 w-maxprc h-maxprc z-10">
+          <dialog open className="bg-vio-mid border-0 border-r-5p pad-2r class">
             <p className="fsize-20p">Now let's add your first word!</p>
             <div className="flex j-center a-center gap-20p">
               <input
                 type="text"
-                ref={input}
-                className="border-3 border-vio-dark border-r-10p pad-1r outline-n fsize-1r bg-vio-light"
+                ref={inputWord}
+                className="border-1 border-r-5p pad-1r outline-n fsize-1r bg-vio-light"
                 placeholder="New epic word"
               />
               <input
                 type="text"
-                ref={input}
-                className="border-3 border-vio-dark border-r-10p pad-1r outline-n fsize-1r bg-vio-light"
-                placeholder="In your native language"
+                ref={inputMeaning}
+                className="border-1 border-r-5p pad-1r outline-n fsize-1r bg-vio-light"
+                placeholder="What does it means?"
               />
-              <button
-                className="border-0 border-r-10p pad-1r bg-vio-dark"
-                onClick={() => inputValidation()}
-              >
+              <button className="border-r-5p pad-1r bg-vio-dark" onClick={() => refreshWords()}>
                 Add
               </button>
             </div>
           </dialog>
         </div>
       )}
-      <div className="flex j-center a-center flex-w-wrap gap-20p">
-        {ReactHtmlParser(data.join(""))}
+      <div className="flex-center flex-w-wrap gap-20p mar-t-2r">
+        {data.map((item, index) => (
+          <div key={index} className="flex-center flex-d-col bg-vio-light h-5r pad-2r">
+            <p className="mar-0 fsize-20p"> {item.word} </p>
+            <p className="mar-0 fsize-08r"> {item.meaning} </p>
+          </div>
+        ))}
       </div>
     </>
   )
